4da98a44f9559ded22ab1893da46dd53f4b7ac5c
Mikhail <mikushnerev@yandex.ru>
2022-01-02 19:51:07 +0300

    hide SECRET_KEY

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -1,5 +1,6 @@
+from dotenv import load_dotenv
+load_dotenv()
 import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -7,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1@q!le=^1mhgi7d=wt7h4)a+cifuq)92su70^0+om3h0@7twkr'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
